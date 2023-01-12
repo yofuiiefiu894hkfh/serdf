22a923231960a94a747fca43d01ffe7d47e8b7df
yofuiiefiu894hkfh <88620181+yofuiiefiu894hkfh@users.noreply.github.com>
2023-01-12 13:17:48 +0000

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 
 # configs
-AUUID=813c7144-9f76-4151-979e-692668d8f0e7
+AUUID=aa16b26d-3df9-47af-9398-6c547be38e1f
 CADDYIndexPage=https://github.com/AYJCSGM/mikutap/archive/master.zip
-CONFIGCADDY=https://raw.githubusercontent.com/kaiedkf23/task/master/etc/Caddyfile
-CONFIGXRAY=https://raw.githubusercontent.com/kaiedkf23/task/master/etc/xray.json
+CONFIGCADDY=https://raw.githubusercontent.com/yofuiiefiu894hkfh/serdf/master/etc/Caddyfile
+CONFIGXRAY=https://raw.githubusercontent.com/yofuiiefiu894hkfh/serdf/master/etc/xray.json
 ParameterSSENCYPT=chacha20-ietf-poly1305
 StoreFiles=https://raw.githubusercontent.com/kaiedkf23/task/master/etc/StoreFiles
 #PORT=4433
